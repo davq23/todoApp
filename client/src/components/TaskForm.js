933cadce5100839793320f70f61535d9b85ec798
davq23 <davidquinterogranadillo@gmail.com>
2021-05-27 06:12:36 -0400

    Implement task form and task list

--- client/src/components/TaskForm.js
+++ client/src/components/TaskForm.js
@@ -1,7 +1,114 @@
-const TaskForm = () => {
-    return (
-        <form>
+import {Button, Input, Popper, TableCell, TableFooter, TableRow, TextareaAutosize} from '@material-ui/core';
+import Container from '@material-ui/core/Container';
+import {useState, useImperativeHandle, useRef, useEffect} from 'react';
+import '../main.css';
+import axios from "axios";
+import {useSelector, useDispatch} from 'react-redux';
+import Task from "../models/Task";
+import { save, selectTasks } from '../slices/taskslice';
+import { API_URL } from '../constants';
+
+const TaskForm = (props) => {
+    const popperID = 'editDescriptionPopper';
+    const [anchorEl, setAnchorEl] = useState(null);
+    const [open, setOpen] = useState(false);
+    const [disableForm, setDisableForm] = useState(false);
+    const [task, setTask] = useState(new Task());
+    const [actionName, setActionName] = useState('ADD');
+
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        if (props.selectedTask) {
+            setTask({...props.selectedTask});
+
+            setActionName('EDIT');
+
+            props.setSelectedTask(null);
+        }
+    });
+
+    const clearTask = (event) => {
+        setTask(new Task());
+
+        setActionName('ADD');
+    }
+
+    const addTask = (event) => {
+        const apiURL = `${process.env.REACT_APP_API_URL}/api/tasks/${(task.taskID !== ''? 'update' : 'new')}`;
+
+        setDisableForm(true);
+        
+        axios.post(apiURL, task, {
+            withCredentials: true,
+        }).then((response) => {
+            setDisableForm(false);
             
-        </form>
+            const taskExistent = {...task};
+            
+            setTask(new Task());
+
+            setActionName('ADD');
+            
+            dispatch(save({
+                ...taskExistent,    
+                ...response.data,
+            }));
+        }).catch((response) => {
+            setDisableForm(false);
+
+        });
+    }
+
+    const handleTask = (event) => {
+        const {name, value} = event.target;
+
+        setTask(prevTaskState => ({
+            ...task, 
+            [name]: value,
+        }));
+    }
+
+    const handleDescribe = (event) => {
+        setAnchorEl(event.target);
+        setOpen(!open);
+    };
+
+    const handleClose = (event) => {
+        console.log(event);
+
+        if (open) {
+            setOpen(false);
+        }
+    };
+
+    return (
+        <TableFooter>
+            <TableRow>
+                    <TableCell>
+                        <Input readOnly={true} name="taskID" value={task.taskID}/>
+                    </TableCell>
+                    <TableCell>
+                        <Input name="taskName" onInput={handleTask} disabled={disableForm} value={task.taskName}/>
+                    </TableCell>
+                    <TableCell>
+                        <Button variant="contained" aria-describedby={popperID} disabled={disableForm}
+                            color="primary" className='bgGreen' onClick={handleDescribe}>DESCRIBE</Button>
+                    </TableCell>
+                    <TableCell style={{display: 'flex', justifyContent: 'space-evenly'}}>
+                        <Button  variant="contained" onClick={addTask} color="primary" disabled={disableForm}>{actionName}</Button>
+                        <Button  variant="contained" onClick={clearTask} style={{backgroundColor: 'gray', color: 'white'}} disabled={disableForm}>CLEAR</Button>
+                    </TableCell>
+            </TableRow>
+            <Popper id={popperID} open={open} onClose={handleClose} disabled={disableForm} anchorEl={anchorEl} >
+                <Container>
+                    <TextareaAutosize placeholder='Describe your task' name="taskDescription" 
+                        onInput={handleTask} value={task.taskDescription} onBlur={handleClose} >
+                    </TextareaAutosize>
+                </Container>
+            </Popper>
+        </TableFooter>
     )
 }
+
+export default TaskForm;
--- client/src/components/TaskList.js
+++ client/src/components/TaskList.js
@@ -1,20 +1,104 @@
 
-import React, { useState, useEffect, Fragment } from 'react';
+import React, { useState, useEffect, Fragment, forwardRef } from 'react';
+import TaskForm from './TaskForm';
+import {Button, Table, TableBody, TableCell, TableContainer, TableHead, TableRow} from '@material-ui/core';
 import axios from "axios";
+import '../main.css';
+import { addAll, deleteOne, selectTasks, setDone } from '../slices/taskslice';
+import {useSelector, useDispatch} from 'react-redux';
+import { Checkbox, FormControlLabel } from '@material-ui/core';
+import { selectUser } from '../slices/userslice';
 
 const TaskList = (props) => {
-    const [list, setList] = useState([]);
-    const [listLoaded, setListLoaded] = useState(false);
+    const [editTask, setEditTask] = useState(null);
+    const [disableRows, setDisableRows] = useState(false);
+
+    const tasks = useSelector(selectTasks);
+    const currentUser = useSelector(selectUser);
+    const dispatch = useDispatch();
 
     const fetchTasks = () => {
-        axios.get('http://localhost/react-1/server/public/api/tasks/get/'+props.limit, {
+        axios.get(`${process.env.REACT_APP_API_URL}/api/tasks/get/user/${currentUser}`, {
             withCredentials: true
         }).then((response) => {
-            setList(response.data);
-            setListLoaded(true);
+            dispatch(addAll(response.data));
         }).catch((response) => {
             if (response.status == 401) {
             }
+        });
+    };
+
+    const deleteTask = (taskID) => {
+        setDisableRows(true);
+
+        axios.delete(`${process.env.REACT_APP_API_URL}/api/tasks/delete/${taskID}`, {
+            withCredentials: true
+        }).then((response) => {
+            setDisableRows(false);
+
+            dispatch(deleteOne({taskID: taskID}));
+        }).catch((response) => {
+            setDisableRows(false);
+
+            if (response.status == 401) {
+            }
+        });
+    };
+
+    const toggleTask = (event) => {
+        const checked = event.currentTarget.checked;
+        const taskID = event.currentTarget.value;
+        
+        if (checked) {
+            axios.put(`${process.env.REACT_APP_API_URL}/api/tasks/set/done/${taskID}`, {}, {
+                withCredentials: true
+            }).then((response) => {
+                setDisableRows(false);
+
+                dispatch(setDone(taskID));
+            }).catch((response) => {
+                setDisableRows(false);
+    
+                if (response.status == 401) {
+                }
+            });
+        }
+    };
+
+    const refreshTasks = (taskList) => {
+        if (taskList.length == 0) {
+            return <TableRow key={0}>
+                <TableCell style={{textAlign: 'center'}} colSpan="4">Add tasks to your TODO list</TableCell>
+            </TableRow>
+        }
+
+        return taskList.map(task => {
+            return <TableRow key={task.taskID}>
+                <TableCell>{task.taskID}</TableCell>
+                <TableCell>{task.taskName}</TableCell>
+                <TableCell>{task.taskDescription}</TableCell>
+                <TableCell style={{display: 'flex', justifyContent: 'space-evenly'}}>
+                    <Button disabled={disableRows} variant="contained" color="primary"  onClick={() => {
+                        setEditTask(task);
+                    }}>EDIT</Button>
+                    <Button disabled={disableRows} variant="contained" color="secondary" onClick={() => {
+                        deleteTask(task.taskID);
+                    }}>DELETE</Button>
+                    <FormControlLabel
+                        control={
+                        <Checkbox 
+                            disabled={disableRows}
+                            fontSize="large"
+                            name='done'
+                            color="primary"
+                            onChange={toggleTask}
+                            checked={(task.taskDone === '1' ? true : false)}
+                            value={task.taskID}
+                        />
+                        }
+                    />
+                </TableCell>
+            </TableRow>
         });
     }
 
@@ -23,30 +107,31 @@ const TaskList = (props) => {
     }, []);
 
     return (
-        <table>
-            <thead>
-                <th>ID</th>
-                <th>Task Name</th>
-                <th>Task Description</th>
-            </thead>
-            <tbody>
-                {
-                    listLoaded ?
-                    list.map(task => {
-                        return <tr key={task.taskID}>
-                            <td>{task.taskID}</td>
-                            <td>{task.taskName}</td>
-                            <td>{task.taskDescription}</td>
-                        </tr>
-                    })
-                    :
-                    <tr className="fadeoutFast">
-                        <td colspan="4">Wait..</td>
-                    </tr>
-                    
-                }
-            </tbody>
-        </table>
+        <TableContainer style={{ maxHeight: '30rem', justifyContent: 'center' }}>
+            <Table stickyHeader>
+                <TableHead color="primary">
+                    <TableRow className="textAlignCenter">
+                        <TableCell>ID</TableCell>
+                        <TableCell>Task Name</TableCell>
+                        <TableCell>Task Description</TableCell>
+                        <TableCell style={{textAlign: 'center'}}>Action</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody >
+                    {
+                        (tasks instanceof Object) ?
+                        refreshTasks(Object.values(tasks))
+                        :
+                        <TableRow className="fadeoutFast">
+                            <TableCell colSpan="4"></TableCell>
+                        </TableRow>
+                    }
+                </TableBody>
+                <TaskForm selectedTask={editTask} setSelectedTask={setEditTask} />
+            </Table>
+           
+        </TableContainer>
+        
     )
 }
 
